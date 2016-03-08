09d7af2726741a95739120854205af308e471cb6
cardoso222 <paulocardoso@infojr.com.br>
2016-03-08 11:44:29 -0300

    Initial Commit

---
+++ cards.js
@@ -0,0 +1,79 @@
+var cards = [
+	
+	//value = 1
+	{"id":1,"name":"Ás","type":"Espadas","image":"images/ace_of_spades.png","value":1},
+	{"id":2,"name":"Ás","type":"Copas","image":"images/ace_of_hearts.png","value":1},
+	{"id":3,"name":"Ás","type":"Ouro","image":"images/ace_of_diamonds.png","value":1},
+	{"id":4,"name":"Ás","type":"Paus","image":"images/ace_of_clubs.png","value":1},
+
+	//value = 2
+	{"id":5,"name":"Duque","type":"Espadas","image":"images/2_of_spades.png","value":2},
+	{"id":6,"name":"Duque","type":"Copas","image":"images/2_of_hearts.png","value":2},
+	{"id":7,"name":"Duque","type":"Ouro","image":"images/2_of_diamonds.png","value":2},
+	{"id":8,"name":"Duque","type":"Paus","image":"images/2_of_clubs.png","value":2},
+
+	//value = 3
+	{"id":9,"name":"Tres","type":"Espadas","image":"images/3_of_spades.png","value":3},
+	{"id":10,"name":"Tres","type":"Copas","image":"images/3_of_hearts.png","value":3},
+	{"id":11,"name":"Tres","type":"Ouro","image":"images/3_of_diamonds.png","value":3},
+	{"id":12,"name":"Tres","type":"Paus","image":"images/3_of_clubs.png","value":3},
+
+	//Value = 4
+	{"id":13,"name":"Quatro","type":"Espadas","image":"images/4_of_spades.png","value":4},
+	{"id":14,"name":"Quatro","type":"Copas","image":"images/4_of_hearts.png","value":4},
+	{"id":15,"name":"Quatro","type":"Ouro","image":"images/4_of_diamonds.png","value":4},
+	{"id":16,"name":"Quatro","type":"Paus","image":"images/4_of_clubs.png","value":4},
+
+	//Value = 5
+	{"id":17,"name":"Cinco","type":"Espadas","image":"images/5_of_spades.png","value":5},
+	{"id":18,"name":"Cinco","type":"Copas","image":"images/5_of_hearts.png","value":5},
+	{"id":19,"name":"Cinco","type":"Ouro","image":"images/5_of_diamonds.png","value":5},
+	{"id":20,"name":"Cinco","type":"Paus","image":"images/5_of_clubs.png","value":5},
+
+	//Value = 6
+	{"id":21,"name":"Seis","type":"Espadas","image":"images/6_of_spades.png","value":6},
+	{"id":22,"name":"Seis","type":"Copas","image":"images/6_of_hearts.png","value":6},
+	{"id":23,"name":"Seis","type":"Ouro","image":"images/6_of_diamonds.png","value":6},
+	{"id":24,"name":"Seis","type":"Paus","image":"images/6_of_clubs.png","value":6},
+
+	//Value = 7 
+	{"id":25,"name":"Sete","type":"Espadas","image":"images/7_of_spades.png","value":7},
+	{"id":26,"name":"Sete","type":"Copas","image":"images/7_of_hearts.png","value":7},
+	{"id":27,"name":"Sete","type":"Ouro","image":"images/7_of_diamonds.png","value":7},
+	{"id":28,"name":"Sete","type":"Paus","image":"images/7_of_clubs.png","value":7},
+
+	//Value = 8
+	{"id":29,"name":"Oito","type":"Espadas","image":"images/8_of_spades.png","value":8},
+	{"id":30,"name":"Oito","type":"Copas","image":"images/8_of_hearts.png","value":8},
+	{"id":31,"name":"Oito","type":"Ouro","image":"images/8_of_diamonds.png","value":8},
+	{"id":32,"name":"Oito","type":"Paus","image":"images/8_of_clubs.png","value":8},
+
+	//Value = 9
+	{"id":33,"name":"Nove","type":"Espadas","image":"images/9_of_spades.png","value":9},
+	{"id":34,"name":"Nove","type":"Copas","image":"images/9_of_hearts.png","value":9},
+	{"id":35,"name":"Nove","type":"Ouro","image":"images/9_of_diamonds.png","value":9},
+	{"id":36,"name":"Nove","type":"Paus","image":"images/9_of_clubs.png","value":9},
+
+	//Value = 10
+	{"id":37,"name":"Dez","type":"Espadas","image":"images/10_of_spades.png","value":10},
+	{"id":38,"name":"Dez","type":"Copas","image":"images/10_of_hearts.png","value":10},
+	{"id":39,"name":"Dez","type":"Ouro","image":"images/10_of_diamonds.png","value":10},
+	{"id":40,"name":"Dez","type":"Paus","image":"images/10_of_clubs.png","value":10},
+	//Espadas
+	{"id":41,"name":"Valete","type":"Espadas","image":"images/jack_of_spades2.png","value":10},
+	{"id":42,"name":"Reis","type":"Espadas","image":"images/king_of_spades2.png","value":10},
+	{"id":43,"name":"Dama","type":"Espadas","image":"images/queen_of_spades2.png","value":10},
+	//Copas
+	{"id":44,"name":"Valetes","type":"Copas","image":"images/jack_of_hearts2.png","value":10},
+	{"id":45,"name":"Reis","type":"Copas","image":"images/king_of_hearts2.png","value":10},
+	{"id":46,"name":"Damas","type":"Copas","image":"images/queen_of_hearts2.png","value":10},
+	//Ouro
+	{"id":47,"name":"Valete","type":"Ouro","image":"images/jack_of_diamonds2.png","value":10},
+	{"id":48,"name":"Reis","type":"Ouro","image":"images/king_of_diamonds2.png","value":10},
+	{"id":49,"name":"Dama","type":"Ouro","image":"images/queen_of_diamonds2.png","value":10},
+	//Paus
+	{"id":50,"name":"Valete","type":"Paus","image":"images/jack_of_clubs2.png","value":10},
+	{"id":51,"name":"Reis","type":"Paus","image":"images/king_of_clubs2.png","value":10},
+	{"id":52,"name":"Dama","type":"Paus","image":"images/queen_of_clubs2.png","value":10},
+	
+	];
---
+++ core.js
@@ -0,0 +1,200 @@
+
+
+$(document).ready(function(){
+
+
+	//utilities
+
+	function randomInt(min,max)
+
+	{
+		return Math.floor(Math.random()*(max-min+1)+min);
+	}
+
+	function inArray(needle, haystack) {
+		var length = haystack.length;
+		for(var i = 0; i < length; i++) {
+			if(haystack[i] == needle) return true;
+		}
+		return false;
+	}
+
+	function generateDeckCards(){
+		var deck = [];
+
+		for (var i = 0; i < cards.length; i++) {
+			card = cards[randomInt(0,52)];
+			if(inArray(card, deck) == false & card != ''){ 
+				deck[i] = card;
+			}
+		};
+		return deck;
+	}
+
+	// Debug Function
+	function showCards(){
+		for (var i = 0; i < cards.length; i++) {
+			console.log()
+			$("#deck").append(cards[i].name + ' de ' +cards[i].type + ' ---- Valor: ' + cards[i].value + '<br>');
+		};
+	}
+
+	function removeFromCards(card){
+
+		for (var i = 0; i < cards.length; i++) {
+			if(cards[i] === card) {
+				cards.splice(i,1);
+				return false;
+			}	
+		}
+	}
+
+
+	function getCardfromDeck(){
+		var number = randomInt(0, cards.length);
+		card = cards[number];
+		return card;
+	}
+
+	//Check the possibilty of success from call on deck
+	function getCallSuccessPossibilities(value){
+		var counter = 0;
+		var allCombinations = 0;
+		var probability = 0;
+		var all = 0;	 
+		var base =  21 - value;
+
+	if(base > 10) {
+
+		for (var i = 0; i < cards.length; i++) {
+			for (var j = 0; j < cards.length; j++) {
+				all = all + 1;
+				if(cards[i].value + cards[j].value === base) {
+					counter++;
+					allCombinations++;
+					probability = allCombinations/all; 
+					console.log(cards[i].name + ' ' + cards[i].type + ' ' + cards[i].value + ' ' + 
+					'Segunda Carta' + cards[j].name + ' ' + cards[j].type + ' ' + cards[j].value);
+				}			
+			}	
+		}	
+	}
+	else{
+		for (var i = 0; i < cards.length; i++) {
+			if(cards[i].value == base){
+				counter++;
+				console.log(cards[i].name + ' ' + cards[i].type + ' ' + cards[i].value);
+			}
+		}
+		probability = counter/cards.length;
+	}
+	return probability;
+}
+
+	function callDeck(player){
+		card = getCardfromDeck()
+		player.push(card);
+		removeFromCards(card);
+		return player;
+	}
+
+	function showPlayer1Cards(player){
+		var totalValue = 0;
+		$("#p1cards").html('');
+		for (var i = 0; i < player.length; i++) {
+			$('#p1cards').append('<img style="height: 100px; width: 80px; margin-right: 80px;" src ='+player[i].image+'>');
+			totalValue = totalValue + player[i].value;
+			player.points = totalValue;
+		};
+			console.log("Total de Pontos:"+	 totalValue);
+	}	
+
+	function showPlayer2Cards(player){
+		var totalValue = 0;
+		$("#p2cards").html('');
+		for (var i = 0; i < player.length; i++) {
+			$('#p2cards').append('<img style="height: 100px; width: 80px; margin-right: 80px;" src ='+player[i].image+'>');
+			totalValue = totalValue + player[i].value;
+			player.points = totalValue;
+		};
+			console.log("Total de Pontos:"+	 totalValue);
+	}	
+	
+	//Check win or lose 
+	function checkStatusofGame(player1, player2){
+
+		if(player1.points == 21){
+			alert('Player 1 Venceu');
+		}
+		if(player2.points == 21){
+			alert('Player 2 Venceu');
+		}
+		if(player1.points > 21){
+			alert('Player 1 Estourou');
+		}
+		if(player2.points > 21){
+			alert('Player 2 Estourou');
+		}
+	}
+
+	var turn = 0;
+	function handle(){
+		if(turn == 0) {
+			console.log('Vez do Player 1');
+			$('#button').css("display","block");
+			turn++;
+		}else{
+			console.log('Vez do Player 2');
+			$('#button').css("display","none");
+			turn--;
+		}
+	}
+
+	// restart from here
+function decisionMaker(player1, player2){
+		var possibility = getCallSuccessPossibilities(player2.points);
+			// fix here
+			if(possibility < 1){
+				console.log(possibility);
+				callDeck(player2);
+				showPlayer2Cards(player2);
+				checkStatusofGame(player1, player2);
+				//handle();	
+			}
+	}
+ 	// finish this 	
+	function setProfile(n){
+		var profiles = [
+		{"id":"1","name":"agressive"},
+		{"id":"2","name":"normal"},
+		{"id":"3","name":"passive"},
+		];
+		return profiles[n];	
+	}
+	
+    // initiate the game and the cards of players
+	var player1 = [];
+	callDeck(player1);
+	callDeck(player1);
+
+	var player2 = [];
+	player2.profile = setProfile(1);
+	callDeck(player2);
+	callDeck(player2);
+
+	// Show Cards 
+	showPlayer1Cards(player1);
+	showPlayer2Cards(player2);
+
+	$('#button').click(function(){
+		callDeck(player1);
+		showPlayer1Cards(player1);
+		checkStatusofGame(player1, player2);
+		handle();
+	});
+
+	decisionMaker(player1,player2);
+	console.table(player2);
+
+
+});
